ec93846ebb7799eb1e1e09c587a646cfcd8f83f5
Vedant Bishnoi <152681427+vedant029@users.noreply.github.com>
2024-07-29 16:45:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ app.get('/', (req, res) => {
 });
 app.listen(port, () => {
   console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Powered By Vedant`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["PLAYING ON MC.REDESKY.TECH","Playing Skyblock!"];
 
 
 let currentIndex = 0;
